b0d6ccabac97eafd8ec3c904282cd126d7192421
Greytiger23 <isabelmothoa49@gmail.com>
2024-04-03 22:28:00 -0700

    airbnb clone v2 deploy static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,12 +6,13 @@ import os
 from fabric.operations import local
 from datetime import datetime
 
+
 def do_pack():
     """generates a.tgz archive from the contents"""
     local("mkdir -p versions")
     t = datetime.utcnow()
     a = "web_static_{}{}{}{}{}{}.tgz".format(t.year, t.month, t.day,
-            t.hour, t.minute, t.second)
+                                             t.hour, t.minute, t.second)
     b = local("tar -cvzf versions/{} web_static".format(a))
     if b.failed:
         return None
