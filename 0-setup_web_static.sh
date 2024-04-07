38a7b939238971ee42349683cad01e6959226aaa
Greytiger23 <isabelmothoa49@gmail.com>
2024-04-06 17:14:03 -0700

    airbnb clone v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
+# check if Nginx installed
 if ! dpkg -l nginx &> /dev/null; then
 	sudo apt-get update
 	sudo apt-get -y install nginx
 fi
+# create directories
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "<html>
+# HTML file for testing
+sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
+<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+</html>
+EOF
+# create symbolic link
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
+# set ownership
 sudo chown -R ubuntu:ubuntu /data/
-c="\\tlocation /hbnb_static {\n\\t\\talias /data/web_static/current/;\n\\t}"
+# configure Nginx
+c="location /hbnb_static {\n\\t\\talias /data/web_static/current/;\n\\t}"
 if ! grep -q "location /hbnb_static" /etc/nginx/sites-available/default; then
 	sudo sed -i "/^server {/a $c" /etc/nginx/sites-available/default
 fi
-sudo service nginx restart
+# check configuration and start
+sudo nginx -t && sudo systemctl restart nginx
 exit 0
